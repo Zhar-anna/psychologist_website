3821fbfb15a81c3601db41de8ab15bb3877ff577
Anna Zharova <a.zharova.muctr@gmail.com>
2023-02-17 16:27:13 +0300

    fix text message

--- src/initView.js
+++ src/initView.js
@@ -26,7 +26,7 @@ export default (watchedState, elements, i18nextInstance) => {
   } = elements;
   watsapp.src = watsapp_image;
   watsapp_href.href =
-    'https://api.whatsapp.com/send?phone=79773513440&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%D0%A5%D0%BE%D1%87%D1%83%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%D1%81%D1%8F%20%D0%BD%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8E.';
+    'https://wa.me/79773513440?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%A5%D0%BE%D1%87%D1%83%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%D1%81%D1%8F%20%D0%BD%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8E.';
   telegram.src = telegram_image;
   photo.src = photo_image;
   vk.href = 'https://vk.com/smeshariki_psy';
